b9f3b5dfa193193dd0f56ab5f13fbd8bab60e3a4
turisap <kirillshakirov57@gmail.com>
2018-08-12 21:17:43 +1200

    Fixes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import Queue from './src/Queue';
 import dotenv from 'dotenv';
-import generator from './src/stressTest';
+//import generator from './src/stressTest';
 
 dotenv.config({ silent: true });
 
-const queue = new Queue([4,5,1,8,3,15,9,10,11,2], true);
+const queue = new Queue([4,5,1,8,3,15,9,10,11,2], false);
 
 console.log(queue.getHeap());
 
-generator();
+//generator();
